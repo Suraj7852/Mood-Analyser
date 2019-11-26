e08e397f5877f2d9bed86e5658697d90e3c89124
Suraj7852 <surajpj7852@gmail.com>
2019-11-26 10:20:38 +0530

    ADDED: given message when sad should return SAD

---
+++ src/test/java/MoodAnalyserTest.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyserTest {
+    @Test
+    public void givenMessageWhenSadShouldReturnSad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is sad message");
+        Assert.assertEquals("SAD",mood);
+
+    }
+}
