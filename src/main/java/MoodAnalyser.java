42a13fea2781218ee349ba73708418174548e700
Suraj7852 <surajpj7852@gmail.com>
2019-11-26 11:00:00 +0530

    REFACTOR: taken mood message in constructor

--- src/main/java/MoodAnalyser.java
+++ src/main/java/MoodAnalyser.java
@@ -1,5 +1,20 @@
 public class MoodAnalyser {
+
+    private String message;
+
+    public MoodAnalyser() {
+    }
+
+    public MoodAnalyser(String message) {
+        this.message = message;
+    }
+
     public String analyseMood(String message) {
+        this.message = message;
+        return analyseMood();
+    }
+
+    public String analyseMood() {
         if(message.contains("sad"))
             return "SAD";
         else
