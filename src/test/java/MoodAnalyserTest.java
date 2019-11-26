ad5b4d39a2455373aee9e7fd682be054cff128b8
Suraj7852 <surajpj7852@gmail.com>
2019-11-26 11:05:05 +0530

    REPEAT TC-1: passed message in constructor

--- src/test/java/MoodAnalyserTest.java
+++ src/test/java/MoodAnalyserTest.java
@@ -4,9 +4,8 @@ import org.junit.Test;
 public class MoodAnalyserTest {
     @Test
     public void givenMessageWhenSadShouldReturnSad() {
-        MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analyseMood("This is sad message");
-        Assert.assertEquals("SAD",mood);
+        MoodAnalyser moodAnalyser = new MoodAnalyser("This is sad message");
+        Assert.assertEquals("SAD",moodAnalyser.analyseMood());
     }
 
     @Test
